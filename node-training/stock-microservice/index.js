268fbffc856e56d375b1f50189a053fd5b1be15a
zan1222 <zanguangni@gmail.com>
2019-08-05 13:12:59 -0700

    updated the code

--- node-training/stock-microservice/index.js
+++ node-training/stock-microservice/index.js
@@ -12,7 +12,7 @@ const server = http.createServer(options);
 const STOCK_API = 'https://clientapi.gcs-web.com/data/7b99bfb2-ab5a-428d-b1dd-d8120d6c3fda';
 //const STOCK_API = 'http://www.pacwest-usa.com/js/lhtpM.js';
 
-function httpRequest(url, request, response){
+function httpRequest(url, options, request, response){
     const reqSent = http.get(url, options, (resRec) => {
          let str = ''
         resRec.on('data', (data) => {
@@ -38,23 +38,33 @@ function httpRequest(url, request, response){
 
 server.on('request', (request, response) => {
     const requestUrl = url.parse(request.url, true);
+    console.log(requestUrl);
     const requestPath = requestUrl.pathname;
 
     switch (requestPath) {
         //when the request URL includes the /eloqua path
         case '/bin/stock-current': {
             // current stock info
-            httpRequest.call(this, `${STOCK_API}/quotes`, request, response);
+            httpRequest.call(this, `${STOCK_API}/quotes`, options, request, response);
             break;
         }
-        case '/bin/stock-lookup': {
+        case '/bin/stock-datelookup': {
+            const date = requestUrl.query.date;
+            console.log(date);
             // historical info lookup
-            //httpRequest.call(this, `${STOCK_API}/quotes`, request, response);
+            // httpRequest.call(this, `${STOCK_API}/quotes/lookup?date=${date}&symbol=NOW`, request, response);
+            httpRequest.call(this, `${STOCK_API}/quotes`, options, request, response);
+            break;
+        }
+        case '/bin/stock-weeklookup': {
+            const date = requestUrl.query.date;
+            // historical info lookup
+            httpRequest.call(this, `${STOCK_API}/quotes/lookup/weekof?date=${date}&symbol=NOW`, request, response);
             break;
         }
         case '/bin/stock-filing': {
             // filings
-            //httpRequest.call(this, `${STOCK_API}/quotes`, request, response);
+            httpRequest.call(this, `${STOCK_API}/filings`, request, response);
             break;
         }
         default:
