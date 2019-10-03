4c12955949d7786bfa206e4883a1c6fc1bdaa0bc
Zan <zan@Zans-MacBook-Pro.local>
2019-10-03 01:19:17 -0700

    created a game

--- react/portfolio/my-app/src/components/App/App.js
+++ react/portfolio/my-app/src/components/App/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 import Clock from '../Clock/Clock';
 import MegaNav from '../MegaNav/MegaNav';
 import FilterableProductTable from '../FilterableProductTable/FilterableProductTable';
@@ -13,13 +11,16 @@ const PRODUCTS = [
   {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
   {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
 ];
+const MyContext = React.createContext('testClass');
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Clock />
         <MegaNav list={['About me', 'my work', 'fun facts']}/>
-        <FilterableProductTable productList={PRODUCTS}/>
+        <MyContext.Provider value={'testClassOne'}>
+          <FilterableProductTable productList={PRODUCTS}/>
+        </MyContext.Provider>
       </header>
     </div>
   );
